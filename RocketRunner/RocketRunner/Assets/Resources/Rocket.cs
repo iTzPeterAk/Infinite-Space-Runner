2cf629a08c8b3323feefde9ff40ad37930ad49b3
iTzPeterAk <55443376+iTzPeterAk@users.noreply.github.com>
2020-02-26 08:42:38 -0500

    made wasd

--- RocketRunner/RocketRunner/Assets/Resources/Rocket.cs
+++ RocketRunner/RocketRunner/Assets/Resources/Rocket.cs
@@ -8,7 +8,6 @@ public class Rocket : MonoBehaviour
     public int currPosIndex = 1;
     public float t = 0;
     public float moveSpeed = 7;
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +17,25 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime * moveSpeed;
+        t = Time.deltaTime * moveSpeed;
         transform.position = Vector3.Lerp(transform.position, rocketPos[currPosIndex], t); 
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            currPosIndex = currPosIndex + 1;
+            if(currPosIndex > rocketPos.Length - 1)
+            {
+                currPosIndex = rocketPos.Length - 1;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            currPosIndex = currPosIndex - 1;
+            if(currPosIndex < 0)
+            {
+                currPosIndex = 0;
+            }
+        }
     }
 }
